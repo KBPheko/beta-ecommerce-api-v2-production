c9e9788399fb56463c24fa48d2ec2a55ffa477ba
unknown <lkgahliso03@gmail.com>
2023-06-25 19:08:37 +0200

    added the orderRepo

--- src/main/java/com/foodapi/betaecommerceapiv2/repository/order/OrderRepository.java
+++ src/main/java/com/foodapi/betaecommerceapiv2/repository/order/OrderRepository.java
@@ -1,4 +1,7 @@
 package com.foodapi.betaecommerceapiv2.repository.order;
 
-public interface OrderRepository {
+import com.foodapi.betaecommerceapiv2.models.order.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order,Long> {
 }
