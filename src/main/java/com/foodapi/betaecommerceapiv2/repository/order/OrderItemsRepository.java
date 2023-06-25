5000228761953d6e280644a169a3a610e3aee930
tsumbedzo <vanessatn96@gmail.com>
2023-06-25 15:27:57 +0200

    added the repo for the OrderModel and extended the model

---
+++ src/main/java/com/foodapi/betaecommerceapiv2/repository/order/OrderItemsRepository.java
@@ -0,0 +1,7 @@
+package com.foodapi.betaecommerceapiv2.repository.order;
+
+import com.foodapi.betaecommerceapiv2.models.order.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemsRepository extends JpaRepository<OrderItem, Integer> {
+}
