dc82f76ce0ebb2b773f441b7303921d938a6e483
tsumbedzo <vanessatn96@gmail.com>
2023-06-25 20:01:22 +0200

    just for test

--- src/main/java/com/foodapi/betaecommerceapiv2/service/order/OrderService.java
+++ src/main/java/com/foodapi/betaecommerceapiv2/service/order/OrderService.java
@@ -1,4 +1,13 @@
 package com.foodapi.betaecommerceapiv2.service.order;
 
+import com.foodapi.betaecommerceapiv2.models.order.OrderItem;
+import com.foodapi.betaecommerceapiv2.repository.order.OrderItemsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public interface OrderService {
+
+    @Autowired
+
+    //calling a fucntion to save an iteam in a particular order
+    void addOrderedProducts(OrderItem orderItem);
 }
--- src/main/java/com/foodapi/betaecommerceapiv2/service/order/OrderServiceImpl.java
+++ src/main/java/com/foodapi/betaecommerceapiv2/service/order/OrderServiceImpl.java
@@ -1,4 +1,15 @@
 package com.foodapi.betaecommerceapiv2.service.order;
 
+import com.foodapi.betaecommerceapiv2.models.order.OrderItem;
+import com.foodapi.betaecommerceapiv2.repository.order.OrderItemsRepository;
+
 public class OrderServiceImpl {
+
+    private OrderItemsRepository orderItemsRepository;
+
+    // method to save an item in a particular order
+    public void addOrderedProducts(OrderItem orderItem) {
+        orderItemsRepository.save(orderItem);
+    }
+git
 }
