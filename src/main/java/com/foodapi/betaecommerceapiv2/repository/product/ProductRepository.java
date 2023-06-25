81b044a2dc83aa920a81bc992029add81566ba48
Karabo Pheko <phekorefi@gmail.com>
2023-06-25 08:44:47 +0200

    Product Repository

--- src/main/java/com/foodapi/betaecommerceapiv2/repository/product/ProductRepository.java
+++ src/main/java/com/foodapi/betaecommerceapiv2/repository/product/ProductRepository.java
@@ -1,4 +1,42 @@
 package com.foodapi.betaecommerceapiv2.repository.product;
 
-public interface ProductRepository {
+import com.foodapi.betaecommerceapiv2.models.product.Category;
+import com.foodapi.betaecommerceapiv2.models.product.Product;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.criteria.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
+
+    /**
+     * Search / Filter functionality which checks data from database and filters according to product name
+     *  and category name
+     */
+
+    default List<Product> searchProduct(String productName, String categoryName){
+
+        return findAll((new Specification<Product>() {
+            @Override
+            public Predicate toPredicate(Root<Product> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+                List<Predicate> predicates = new ArrayList<>();
+
+                if (productName != null) {
+                    predicates.add(criteriaBuilder.like(criteriaBuilder.lower(root.get("name")), "%" + productName.toLowerCase() + "%" + productName.toUpperCase() + "%"));
+                }
+                if (categoryName != null) {
+                    Join<Product, Category> prodCategoryJoin = root.join("category");
+                    predicates.add(criteriaBuilder.like(criteriaBuilder.lower(prodCategoryJoin.get("categoryName")), "%" + categoryName.toLowerCase() + "%" + categoryName.toUpperCase() + "%" ));
+                }
+
+                return criteriaBuilder.and(predicates.toArray( new Predicate[predicates.size()]));
+            }
+        }));
+    }
+
 }
