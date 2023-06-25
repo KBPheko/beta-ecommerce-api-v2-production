82cee7a38a7f1d063897545c0eda137c44f3ed88
Karabo Pheko <phekorefi@gmail.com>
2023-06-25 07:58:31 +0200

    added Product and Category Models

--- src/main/java/com/foodapi/betaecommerceapiv2/models/product/Category.java
+++ src/main/java/com/foodapi/betaecommerceapiv2/models/product/Category.java
@@ -1,4 +1,60 @@
 package com.foodapi.betaecommerceapiv2.models.product;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "category")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoryId;
+
+    @Column(name = "categoryName")
+    private String categoryName;
+
+    @OneToMany(mappedBy = "category", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Product> products;
+
+    // default constructor
+    public Category() {
+    }
+
+    //parameterized constructor
+
+    public Category(Long categoryId, String categoryName, List<Product> products) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+        this.products = products;
+    }
+
+    // Getters and Setters
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
 }
