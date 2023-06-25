615e212dbd0ea3290192256b64cf31097735a64a
tsumbedzo <vanessatn96@gmail.com>
2023-06-25 19:31:16 +0200

    cart model with the mapped cartItem and List<> withing the consructor

--- src/main/java/com/foodapi/betaecommerceapiv2/models/cart/Cart.java
+++ src/main/java/com/foodapi/betaecommerceapiv2/models/cart/Cart.java
@@ -1,4 +1,89 @@
 package com.foodapi.betaecommerceapiv2.models.cart;
 
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "cart")
 public class Cart {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "custome_id")
+    private Long customerId;
+
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    @Column(name = "totalPrice")
+    private Double totalPrice;
+
+    @OneToOne(mappedBy = "cart", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private List<CartItem> cartItem;
+
+    public Cart(){
+
+    }
+    public Cart(long id, Long customerId, Date createdAt, Date updatedAt, Double totalPrice, List<CartItem> cartItem){
+        this.id = id;
+        this.customerId = customerId;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.cartItem = cartItem;
+        this.totalPrice = totalPrice;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(Double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public List<CartItem> getCartItem() {
+        return cartItem;
+    }
+
+    public void setCartItem(List<CartItem> cartItem) {
+        this.cartItem = cartItem;
+    }
 }
