b8d0b54862a2d6e25a922aab0792a929bae56e1e
unknown <lkgahliso03@gmail.com>
2023-06-26 02:16:50 +0200

    updated the order repo

--- src/main/java/com/foodapi/betaecommerceapiv2/repository/order/OrderRepository.java
+++ src/main/java/com/foodapi/betaecommerceapiv2/repository/order/OrderRepository.java
@@ -3,5 +3,8 @@ package com.foodapi.betaecommerceapiv2.repository.order;
 import com.foodapi.betaecommerceapiv2.models.order.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order,Long> {
+    List<Order> findAllByCustomer(String customer);
 }
