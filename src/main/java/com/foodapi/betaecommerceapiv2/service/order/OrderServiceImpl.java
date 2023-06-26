7fa017ae9d5ff61738c2b23f142191ee101f8c94
unknown <lkgahliso03@gmail.com>
2023-06-26 02:44:30 +0200

    added functions to the serviceImpl file

--- src/main/java/com/foodapi/betaecommerceapiv2/service/order/OrderServiceImpl.java
+++ src/main/java/com/foodapi/betaecommerceapiv2/service/order/OrderServiceImpl.java
@@ -1,4 +1,73 @@
 package com.foodapi.betaecommerceapiv2.service.order;
 
-public class OrderServiceImpl {
+
+
+import com.foodapi.betaecommerceapiv2.models.order.Order;
+import com.foodapi.betaecommerceapiv2.models.order.OrderItem;
+import com.foodapi.betaecommerceapiv2.models.product.Product;
+import com.foodapi.betaecommerceapiv2.repository.order.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * This class implements the methods that provides CRUD operations for the order table
+ */
+@Service
+public class OrderServiceImpl implements OrderService {
+
+   // private final CartService cartService;
+
+    private final OrderRepository orderRepository;
+
+    private final OrderItemRepository orderItemRepository;
+
+    @Autowired
+    public OrderServiceImpl(OrderRepository orderRepository, OrderItemRepository orderItemRepository) {
+        //this.cartService = cartService;
+        this.orderRepository = orderRepository;
+        this.orderItemRepository = orderItemRepository;
+    }
+
+    // This method checks out a user's cart items and creates an order
+    @Async
+    public void checkOut(String customer) {
+        //CompletableFuture<CartDto> cartDto = cartService.listCartItems(customer);
+
+        //List<CartItemDto> cartItemDtoList = cartDto.join().getCartItems();
+
+        Order order = new Order();
+        order.setCreatedDate(new Date());
+        //order.setUpdatedAt(new Date());
+        order.setCustomer(customer);
+        order.setTotalAmount(cartDto.join().getTotal());
+        orderRepository.save(order);
+
+       // for (CartItemDto cartItemDto : cartItemDtoList) {
+            OrderItem orderItem = new OrderItem();
+            orderItem.setCreatedAt(new Date());
+            orderItem.setPrice(Product.getPrice());
+            orderItem.setQuantity(cartItemDto.getQuantity());
+            orderItem.setOrder(order);
+            orderItemRepository.save(orderItem);
+       // }
+        //cartService.deleteUserCartItems(customer);
+    }
+
+    // This method returns a list of orders for a particular user
+    @Async
+    public CompletableFuture<List<Order>> getAllOrders(String customer) {
+        return CompletableFuture.completedFuture(orderRepository.findAllByCustomer(customer));
+    }
+
+    // This method returns a particular order
+    @Async
+    public CompletableFuture<Order> getOrder(Long id) throws OrderNotFoundException {
+        CompletableFuture<Order> order = CompletableFuture.completedFuture(orderRepository.findById(id).orElseThrow(() -> new OrderNotFoundException("Order not found")));
+        return CompletableFuture.completedFuture(order.join());
+    }
 }
