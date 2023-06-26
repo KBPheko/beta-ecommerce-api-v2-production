ae066ef958825b61809b2a7a9f6a29fe3c4e45d4
NyikoCamango <217026744@student.uj.ac.za>
2023-06-26 10:22:08 +0200

    the cartItemDto makes use of the cart model

--- src/main/java/com/foodapi/betaecommerceapiv2/models/cart/dto/CartItemDto.java
+++ src/main/java/com/foodapi/betaecommerceapiv2/models/cart/dto/CartItemDto.java
@@ -1,4 +1,12 @@
 package com.foodapi.betaecommerceapiv2.models.cart.dto;
 
+import com.foodapi.betaecommerceapiv2.models.product.Product;
+
 public class CartItemDto {
+    private Long id;
+
+    private Integer quantity;
+
+    private Product product;
+
 }
