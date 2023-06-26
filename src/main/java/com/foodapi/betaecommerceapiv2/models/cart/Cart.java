fc93a515e3b28e18353d5a86d54cfe5e8688eb45
NyikoCamango <217026744@student.uj.ac.za>
2023-06-26 10:14:00 +0200

    added the getters and setters, together with creating the cart entity in the database

--- src/main/java/com/foodapi/betaecommerceapiv2/models/cart/Cart.java
+++ src/main/java/com/foodapi/betaecommerceapiv2/models/cart/Cart.java
@@ -1,10 +1,11 @@
 package com.foodapi.betaecommerceapiv2.models.cart;
 
 
+import com.foodapi.betaecommerceapiv2.models.product.Product;
 import io.swagger.v3.oas.annotations.media.Schema;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 /**
  * This class defines the cart model
@@ -13,5 +14,89 @@ import javax.persistence.Table;
 @Schema(name = "Cart", description = "Cart model")
 @Table(name = "cart")
 public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "Cart id", example = "1")
+    private Long id;
 
+    @Column(name = "created_at")
+    @Schema(description = "Cart created at", example = "2021-09-01 00:00:00")
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    @Schema(description = "Cart updated at", example = "2021-09-01 00:00:00")
+    private Date updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Column(name = "customer")
+    @Schema(description = "Cart customer", example = "abc123@gmail.com")
+    private String customer;
+
+    @Column(name = "quantity")
+    @Schema(description = "Cart quantity", example = "1")
+    private Integer quantity;
+
+
+    public Cart() {
+        super();
+    }
+
+    public Cart(Product product, String customer, Integer quantity) {
+        this.createdAt = new Date();
+        this.updatedAt = new Date();
+        this.product = product;
+        this.customer = customer;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 }
