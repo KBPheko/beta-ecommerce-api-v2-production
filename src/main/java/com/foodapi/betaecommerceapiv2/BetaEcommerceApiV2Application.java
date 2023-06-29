261c6eb8a68ce40212c94199e8fdf93f68f4efae
KBPheko <@J8riW2aG*>
2023-06-29 16:32:26 +0200

    Production code

--- src/main/java/com/foodapi/betaecommerceapiv2/BetaEcommerceApiV2Application.java
+++ src/main/java/com/foodapi/betaecommerceapiv2/BetaEcommerceApiV2Application.java
@@ -3,10 +3,12 @@ package com.foodapi.betaecommerceapiv2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication (exclude = {SecurityAutoConfiguration.class})
 public class BetaEcommerceApiV2Application {
 
+	@Profile("prod")
 	public static void main(String[] args) {
 		SpringApplication.run(BetaEcommerceApiV2Application.class, args);
 		System.out.print("Server connected at localhost:8080 ...");
